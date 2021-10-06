f5706351d9caaa580aa3da5f126ca0f94c6305cb
Steven (Siwei) Ye <stevenysw@berkeley.edu>
2021-10-05 22:08:22 -0700

    Update strat_full.R

--- method/strat_full.R
+++ method/strat_full.R
@@ -3,12 +3,13 @@
 ## X: covariates
 ## y: outcome variable
 ## z: treatment indicator
+## K: number of subgroups
 
 # Outputs:
 ## tau: estimation of treatment effects
 
 
-strat_full <- function(X, y, z){
+strat_full <- function(X, y, z, K){
   control = which(z == 0)
   treatment = which(z == 1)
   
@@ -16,12 +17,12 @@ strat_full <- function(X, y, z){
   beta_hat_p = temp$coefficients
   p_hat = drop(cbind(rep(1, length(y)), X) %*% beta_hat_p)
   
-  itv = c(min(p_hat)-1, quantile(p_hat, c(1/3, 2/3)), max(p_hat) + 1)
+  itv = c(min(p_hat)-1, quantile(p_hat, (1:(K-1))/K), max(p_hat) + 1)
   
   group = as.numeric(cut(p_hat, itv))
   
-  tau_group = rep(0, 3)
-  for (i in 1:3){
+  tau_group = rep(0, K)
+  for (i in 1:K){
     tau_group[i] = sum(y[group == i & z == 1]) / sum(as.numeric(group == i & z == 1)) - sum(y[group == i & z == 0]) / sum(as.numeric(group == i & z == 0))
   }
   
