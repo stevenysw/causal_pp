d0f082d451aada6ff28266dc00740dc7411195c8
Steven (Siwei) Ye <stevenysw@berkeley.edu>
2022-09-12 05:51:03 -0700

    Update sim_code.R

--- simulations/sim_code.R
+++ simulations/sim_code.R
@@ -373,3 +373,19 @@ boxplot(mse_ca, mse_st, mse_cf,
         names = c("PP", "ST", "CF"), 
         ylab = "MSE", main = "Scenario 7, d = 5000, n = 3000")
 
+# nmes
+smoke <- read.csv("nmesdata.txt", header = T)
+smoke <- subset(smoke, TOTALEXP > 0)
+smoke <- subset(smoke, select = c(TOTALEXP, packyears, LASTAGE, AGESMOKE,
+                    MALE, RACE3, marital, educate,
+                     SREGION, POVSTALB, beltuse, yearsince))
+smoke <- na.omit(smoke)
+smoke$weight <- nmes_data[row.names(smoke),]$HSQACCWT
+smoke <- na.omit(smoke)
+
+X = as.matrix(smoke[,-c(1,2)])
+y = log(smoke$TOTALEXP)
+z = as.numeric(smoke$packyears > 17)
+
+rpart.plot(tau_cart$model)
+
